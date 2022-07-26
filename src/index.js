90970a114107b48ade2a9f381ece9a995abb296f
laurisz94 <lauriisz1994@gmail.com>
2022-07-25 23:10:43 -0500

    listo invalid

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import validator from './validator.js';
 
 // conectar el dom con la funcionalida logica
 const boton = document.getElementById("boton");
+const creditCard = document.getElementById('numeroTarjeta').value;
 //const creditCard = document.getElementById("numeroTarjeta").value;
 
 //Es para que funcione el boton al dar click y crea la variable para traer el valor que ponga el usario
 boton.addEventListener('click',function(e){
    e.preventDefault();
    const creditCard = document.getElementById('numeroTarjeta').value;
-   validator.isValid(creditCard);
- //console.log(creditCard)
-
-   //numeroCredit = validator.isValid(numeroCredit);
-   //mostrarValidacion(numeroCredit)Ho
-})
+   
+   if (validator.isValid (creditCard) === true) {
+    alert("Tarjeta válida")
+  }
+    else { 
+      alert("Tarjeta invalida")
+    }
+   //validator.isValid(creditCard);
+}) 
 
 //console.log(validator);
-
--- src/validator.js
+++ src/validator.js
@@ -5,50 +5,53 @@ isValid : function (creditCard) {//la funcion invertir numero
     let arrayNumber = creditCard.split('').reverse().join('');
     let validacionCardNumber = [] 
 //console.log(arrayNumber);
-
+//let reverseAarrayNumber = validacionCardNumber
 //Guardar los valores en un array ya al areves
 for (let i=0; i < creditCard.length; i++) { //se cra un ciclo for que va desde la posicion 0 hasta la 16 
-    validacionCardNumber.push(parseInt(arrayNumber[i])); 
-//console.log(arrayNumber)
-}
+    validacionCardNumber.push(parseInt(arrayNumber[i]));
+    } 
 
 //length dice el numero de elementos que tiene 
 
 //Iniciar el algoritmo de LUhm
 let reverseAarrayNumber = validacionCardNumber//Es para guardar el valor que da en los array 
-    for (let i = 0; i <= reverseAarrayNumber.length; i++) {//que sea menor a ese numero de elmentos 16
-        let posicion = i + 1; //se crea un variable donde sumamos 1 posion para que se tome la 1era posicion en el array.
-        let posicionPar = posicion % 2; //va dividir el numero de la posición sobre 2 y el residuio debe dar 0 
-      if (posicionPar===0) {let valor = reverseAarrayNumber[i];
-                            let valor2 = valor * 2;
-                            reverseAarrayNumber = valor2}
-                        
-                        else {reverseAarrayNumber[i]*2 ;}
-        
-console.log(reverseAarrayNumber)
 
-//if(posicionPar ===0) { //Si el valor del residuo es igual a 0 es par 
-    //let valor = reverseAarrayNumber[i] // se guarda el valor de los numeros ivertidos en suposicion par
-    //let multiplicacion = valor * 2; // el valor se multiplica psor 2 
+//console.log(reverseAarrayNumber)
+let numero = 0
+    for (let i = 1; i < reverseAarrayNumber.length; i = i + 2) {//aqui ya tengo las posiciones que van ser pares 
+   let multiplicacion = 2 * reverseAarrayNumber[i];  
+    //reverseAarrayNumber[i] = multiplicacion
+ //numero = numero + reverseAarrayNumber[i]
 
-    //console.log(valor)
 
-//if (multiplicacion >=9){//si el valor de la multiplicacion es mayor a 10 se suma los 2 numeros pero s edeben romper
-    //let valor2 = multiplicacion - 9;
+if (multiplicacion >9){//si el valor de la multiplicacion es mayor 9 se resta. -9 y si no no hace nada
+    reverseAarrayNumber [i] = multiplicacion - 9;
 }
 
-//else {
-    //let valor3 = multiplicacion; 
-   // console.log();
+else {
+    reverseAarrayNumber[i]= multiplicacion
+}
+
+//console.log(reverseAarrayNumber)
+} 
+
+let suma = reverseAarrayNumber.reduce((numero, total)=> numero += total, 0);
+//console.log(suma)
+
+if (suma % 10 === 0){
+    console.log("valida");
+    return true
+}
+    else {
+        console.log ("invalida");
+        return false
+    }
+
+//Si la condicion dice que la posicion par es igual a 0 es par    
 }
 
 }
-
-
-//} //Si la condicion dice que la posicion par es igual a 0 es par    
-//}
-
 
 //Obtener los valores de los numeros en las posiciones pares 
 // tener toda la logica del algoritmo
-export default validator;
+export default validator
